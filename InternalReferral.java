a817fc868d8defb7f59b9adda3dedb0ac2ca6300
HarrisAllwin <harrisallwin25@gmail.com>
2016-07-24 11:38:32 +0530

    Add files via upload

---
+++ InternalReferral.java
@@ -0,0 +1,11 @@
+package com.wipro.ips.entity;
+
+public class InternalReferral {
+	public InternalReferral (int applicantId,String applicantName,
+			int yrsOfExp, String skills, double expSalary, int employeeId){
+	}
+		int employeeId;
+	}
+	
+
+
